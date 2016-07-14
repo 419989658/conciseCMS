eb745e4f784546ff51dd6cb0062dcb8d7aac5cfe
kevenliu <kevenliu@beva.com>
2016-07-14 20:54:56 +0800

    1.删除测试文件 2.添加基本文件结构

---
+++ vendor/yiisoft/yii2/BaseYii.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: keven
+ * Date: 2016/7/14
+ * Time: 20:38
+ */
+namespace yii;
+
+class BaseYii
+{
+    /**
+     * @var Container 使用[[createObject()]]封装依赖注入
+     */
+    public static $container;
+}
---
+++ vendor/yiisoft/yii2/yii.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: keven
+ * Date: 2016/7/14
+ * Time: 20:38
+ */
+
+require (__DIR__.'/BaseYii.php');
+
+class Yii extends \yii\BaseYii
+{
+
+}
