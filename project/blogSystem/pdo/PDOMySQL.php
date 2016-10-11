65b1d3bdf6bc998de626a52a4f07a4ee25fe6127
sometimes <lq8841149@163.com>
2016-10-11 23:59:45 +0800

    封装PDO getAll 和query

--- project/blogSystem/pdo/PDOMySQL.php
+++ project/blogSystem/pdo/PDOMySQL.php
@@ -5,6 +5,7 @@
  * Date: 2016/10/11
  * Time: 20:08
  */
+require 'config.php';
 class PDOMySQL
 {
     public static $connectConfig = [];//设置连接参数的配置信息
@@ -12,9 +13,17 @@ class PDOMySQL
     public static $pConnect = false;//是否开启长连接
     public static $dbVersion = null;//保存数据库版本
     public static $isConnected = false;// 是否连接成功
+    public static $pdoStatement = null; // 返回PDOStatementd对象
+    public static $sqlStr = null; //最后执行的SQL语句
+
+    /**
+     * PDOMySQL constructor.
+     * @param string $dbConfig
+     */
     public function __construct($dbConfig='')
     {
-        if(class_exists("PDO")){
+        header("Content-type:text/html;charset=utf-8");
+        if(!class_exists("PDO")){
             self::throw_exception('不支持PDO,请先开启');
         }
         if(!is_array($dbConfig)){
@@ -25,7 +34,7 @@ class PDOMySQL
                 'dbname'=>DB_NAME,
                 'hostport'=>DB_PORT,
                 'dbms'=>DB_TYPE,
-                'dsn'=>DB_TYPE.':host='.DB_HOST.'dbname='.DB_NAME,
+                'dsn'=>DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME.';port='.DB_PORT,
             ];
         }
         if(empty($dbConfig['hostname'])) self::throw_exception('没有定义数据库配置');
@@ -42,7 +51,6 @@ class PDOMySQL
                     self::$connectConfig['password'],
                     self::$connectConfig['params']);
             }catch (PDOException $e){
-
                 self::throw_exception($e->getMessage()) ;
             }
             if(!self::$instance){
@@ -50,10 +58,45 @@ class PDOMySQL
                 return false;
              }
              self::$instance->exec("SET NAMES ".DB_CHARSET);
-             self::$dbVersion = self::$instance->getAttribute(contant("PDO::ATTR_SERVICE_VERSION"));
+             self::$dbVersion = self::$instance->getAttribute(constant("PDO::ATTR_SERVER_VERSION"));
             self::$isConnected = true;
             unset($dbConfig);
         }
+    }
+
+    public static function getAll($sql = null)
+    {
+        if($sql != null){
+            self::query($sql);
+        }
+        $result = self::$pdoStatement->fetchAll(constant("PDO::FETCH_ASSOC"));
+        return $result;
+    }
+
+    public static function query($sql = '')
+    {
+        $instance = self::$instance;
+        if(!$instance) return false;
+        if(!empty(self::$pdoStatement)) self::free(); //释放上一次操作遗留的结果集
+        self::$sqlStr = $sql;
+        self::$pdoStatement = self::$instance->prepare(self::$sqlStr);
+        $res = self::$pdoStatement->execute();
+        self::haveErrorThrowException();
+        return $res;
+    }
+
+    public static function haveErrorThrowException()
+    {
+        $obj = empty(self::$pdoStatement)?self::$pdoStatement:self::$instance;
+        $arrError = $obj->errorInfo();
+        print_r($arrError);
+    }
+    /**
+     * 释放结果集
+     */
+    public static function free()
+    {
+        self::$pdoStatement = null;
     }
 
     /**
@@ -62,6 +105,12 @@ class PDOMySQL
      */
     public static function throw_exception($errMsg)
     {
-            echo "<div style='width:80%;background-color:#ABCDEF;color:black;font-size:16px'>{$errMsg}</div>";
+        echo "<div style='width:80%;background-color:#ABCDEF;color:black;font-size:16px'>{$errMsg}</div>";
     }
+
 }
+
+$pdoMysql = new PDOMySQL();
+$sql = 'select * from test';
+$pdoMysql::getAll($sql);
+print_r($pdoMysql);
---
+++ project/blogSystem/pdo/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: sometimes
+ * Date: 2016/10/11
+ * Time: 23:25
+ */
+define("DB_HOST",'localhost');
+define("DB_USER",'root');
+define("DB_PWD",'root');
+define("DB_NAME",'db_blog');
+define("DB_PORT",'3307'); //端口号
+define("DB_TYPE",'mysql');//数据库类型
+define("DB_CHARSET",'utf8'); //字符类型
